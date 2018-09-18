a4387214926d91ecf84db4c7cc474a710b46f2b2
Pushpendre Rastogi <prastog3@jhu.edu>
2018-09-18 16:49:38 -0400

    finally figured out that the checkpoints and logs were somehow causing a problem. And also fixed the incompatibiltity btw qsub.sh and expt.sh

--- expt.sh
+++ expt.sh
@@ -3,6 +3,7 @@ DEBUG=${2-echo}
 
 wgan () {
   for dim in 300 600 900 1200 1500 1800 2100; do
+    $DEBUG rm -rf wganexpt/checkpoints wganexpt/logs
     CUDA_VISIBLE_DEVICES=$(free-gpu) $DEBUG ./expt.py wgan_mog_conv --num_epochs 10000 --sample_interval 250 --dis_dim $dim --plot_pfx "./wganexpt/fig/wgan.dim$dim.epoch{epoch}.png"
   done
 }
