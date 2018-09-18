ce8702d16573107da49b1eda62c18f21fe47861d
Pushpendre Rastogi <prastog3@jhu.edu>
2018-09-18 15:58:42 -0400

    add ability to qsub scripts

--- expt.sh
+++ expt.sh
@@ -3,9 +3,14 @@ DEBUG=${2-echo}
 
 wgan () {
   for dim in 300 600 900 1200 1500 1800 2100; do
-    CUDA_VISIBLE_DEVICES=$(free-gpu) $DEBUG ./expt.py wgan_mog_conv --num_epochs 10000 --sample_interval 250 --dis_dim $dim
+    CUDA_VISIBLE_DEVICES=$(free-gpu) ./expt.py wgan_mog_conv --num_epochs 10000 --sample_interval 250 --dis_dim $dim
   done
 }
 
+wganqsub () {
+    for dim in 300 600 900 1200 1500 1800 2100; do
+	$DEBUG qsub -V -j y -l gpu=1 -cwd ./qsub.sh $dim
+    done
+}
 
 $FNC
---
+++ qsub.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=$(free-gpu) ./expt.py wgan_mog_conv --num_epochs 10000 --sample_interval 250 --dis_dim $1
